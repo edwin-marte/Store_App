ae63521b55432da7606bdba68872ef0090b9dad3
Edwin <edwin25mt@gmail.com>
2023-05-22 10:40:17 -0400

    Create sealed class for state management

---
+++ app/src/main/java/com/example/store_app/core/Resource.kt
@@ -0,0 +1,7 @@
+package com.example.store_app.core
+
+sealed class Resource<out T> {
+    class Loading<out T> : Resource<T>()
+    data class Success<out T>(val data: T) : Resource<T>()
+    data class Failure<out T>(val exception: Exception) : Resource<T>()
+}
