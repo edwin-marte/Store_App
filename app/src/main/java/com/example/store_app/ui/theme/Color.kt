4ece395b3857c028f9c60ef6dbc498dd0cab8ee6
Edwin <edwin25mt@gmail.com>
2023-05-22 10:35:06 -0400

    Initial commit

---
+++ app/src/main/java/com/example/store_app/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.store_app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val LightGray = Color(0xFFF1F1F1)
+val SkyBlue = Color(0xFF34B2EB)
